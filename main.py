2d92c5253dfe14519a6be4bf560a94c90934f3b9
cu-sp <down1995@tutanota.com>
2024-04-26 13:49:38 -0400

    added buy and sell methods

--- main.py
+++ main.py
@@ -3,4 +3,22 @@ import alpaca_trade_api as tradeapi
 SEC_KEY = ''
 PUB_KEY = ''
 BASE_URL = ''
+api = tradeapi.REST(key_id=PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL) #base url only used in paper trading
 
+
+def buy(ticker,amount): #buy stock   
+    api.submit_order(
+        symbol= ticker #stock ticker
+        qty=amount
+        side='buy'
+        type='market'
+        time_in_force='gtc' #good til canceled
+    )
+def sell(ticker,amount): #sell stock
+    api.submit_order(
+        symbol=ticker
+        qty=amount
+        side='sell'
+        type='market'
+        time_in_force='gtc'
+    )
