a1364edacf87eb84912ab70eb9a8c94a0c10cf03
cu-sp <down1995@tutanota.com>
2024-04-27 12:30:27 -0400

    Add main() and stock info getter

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ def sell(ticker,amount): #sell stock
         type='market'
         time_in_force='gtc'
     )
+
+def main():
+    #Following code gets and displays market data for stock symb.
+    symb = 'SPY' #ticker 
+    while True:
+        print("")
+        print("Checking Market Price")
+        market_data = api.get_barset(symb, 'minute', limit=5) #bar data for past 5 minutes
+        close_list = []
+        for bar in marketData[symb]:
+            close_list.append(bar.c) #bar.c is the bars closing price at time interval 
+
+    close_list = np.array(close_list, dtype=np.float64)
+    moving_average = np.mean(close_list)
+    last_price = close_list[4] 
+    print("Moving Average: " + str(moving_average))
+    print("Last Price: " + str(last_price))
+    time.sleep(60)
