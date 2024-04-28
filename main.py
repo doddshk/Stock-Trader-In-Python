9152597629f64852dae7adf305b5a2bfb0534905
cu-sp <down1995@tutanota.com>
2024-04-28 13:08:57 -0400

    Bug fixes

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import alpaca_trade_api as tradeapi
 
 SEC_KEY = ''
 PUB_KEY = ''
-BASE_URL = ''
+BASE_URL = 'https://paper-api.alpaca.markets/v2'
 api = tradeapi.REST(key_id=PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL) #base url only used in paper trading
 symb = 'SPY' #ticker 
 pos_held = False
 
-def buy(ticker,amount): #buy stock   
+def buy(ticker): #buy stock   
     api.submit_order(
-        symbol= ticker #stock ticker
-        qty=amount
-        side='buy'
-        type='market'
+        symbol= ticker, #stock ticker
+        qty=1,
+        side='buy',
+        type='market',
         time_in_force='gtc' #good til canceled
     )
-def sell(ticker,amount): #sell stock
+def sell(ticker): #sell stock
     api.submit_order(
-        symbol=ticker
-        qty=amount
-        side='sell'
-        type='market'
+        symbol=ticker,
+        qty=1,
+        side='sell',
+        type='market',
         time_in_force='gtc'
     )
 
@@ -29,23 +29,24 @@ def main():
     while True:
         print("")
         print("Checking Market Price")
-        market_data = api.get_barset(symb, 'minute', limit=5) #bar data for past 5 minutes
+        market_data = api.get_bars(symb, 'minute', limit=5) #bar data for past 5 minutes
         close_list = []
         for bar in marketData[symb]:
             close_list.append(bar.c) #bar.c is the bars closing price at time interval 
 
-    close_list = np.array(close_list, dtype=np.float64)
-    moving_average = np.mean(close_list)
-    last_price = close_list[4] 
-    print("Moving Average: " + str(moving_average))
-    print("Last Price: " + str(last_price))
-    time.sleep(60)
+        close_list = np.array(close_list, dtype=np.float64)
+        moving_average = np.mean(close_list)
+        last_price = close_list[4] 
+        print("Moving Average: " + str(moving_average))
+        print("Last Price: " + str(last_price))
 
-    if ma+0.1 < last_price and not pos_held:
-        print("buy")
-        buy(ticker,1)
-        pos_held = True
-    elif ma-0.1 < last_price and pos_held:
-        print("sell")
-        sell(ticker,1)
-        pos_held = False 
+        if moving_average+0.1 < last_price and not pos_held:
+            print("buy")
+            buy()
+            pos_held = True
+        elif moving_average-0.1 < last_price and pos_held:
+            print("sell")
+            sell(symb)
+            pos_held = False 
+        time.sleep(60)
+main()
